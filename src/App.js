7c1c9291c638a6531bfe3716d6bd3167138fe816
Anouarharchi <anwarharchi003@gmail.com>
2025-01-09 00:04:06 +0100

    v1.04

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-//import :
-// import Hello from "./react_exemples/Hello";
 import React from "react";
-import {BrowserRouter , Routes ,Route}from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./app_react-crud/Home";
 import Create from "./app_react-crud/create";
 import Read from "./app_react-crud/read";
@@ -10,20 +8,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-<>
-{/* component: */}
-{/* <Hello/> */}
-<BrowserRouter>
-
-<Routes >
-  <Route path="/home" element={<Home/>}></Route>
-  <Route path="/create" element={<Create/>}></Route>
-  <Route path="/update/:id" element={<Update/>}></Route>
-  <Route path="/read/:id" element={<Read/>}></Route>
-</Routes>
-
-</BrowserRouter>
-</>
+    <>
+      {/* component: */}
+      {/* <Hello/> */}
+      <Router basename="/CRUD_REACT"> 
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update/:id" element={<Update />} />
+          <Route path="/read/:id" element={<Read />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
